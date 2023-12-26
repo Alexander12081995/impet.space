2ee450c1ad826e58ab6c391d9086ec0b3f32f192
Парковский Александр Викторович <a.parkouski.frontend@gmail.com>
2023-12-27 01:02:53 +0300

    add block review

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,6 +5,7 @@ import css from "./Main.module.css";
 import { Motivation } from "@/components/Main/Motivation/Motivation";
 import { PhotoBlock } from "@/components/Main/PhotoBlock/PhotoBlock";
 import { Apply } from "@/components/Main/Apply/Apply";
+import { Reviews } from "./Reviews/Reviews";
 
 export const Main = ({ params }: { params: { locale: string } }) => {
     return (
@@ -14,6 +15,7 @@ export const Main = ({ params }: { params: { locale: string } }) => {
             <Motivation />
             <PhotoBlock />
             <Counter />
+            <Reviews />
             <Apply />
         </div>
     );
